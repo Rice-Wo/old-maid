198dec0ad0a93e1cf15bcb087ab0446ead4bf628
Rice-Wo <yeeho2006@gmail.com>
2022-11-05 17:49:30 +0800

    update chat

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 import random
 import asyncio
 import requests
+import re
 
 bot = discord.Bot(debug_guilds=[911190180260626453],status=discord.Status.do_not_disturb, intents = discord.Intents().all())
 
@@ -17,7 +18,7 @@ with open('Token.json', 'r', encoding = "utf-8") as token:
 	token = json.load(token)
 
 with open('chat.json', 'r', encoding = "utf-8") as chat:
-	chat = json.load(chat)
+	chat_data = json.load(chat)
 
 
 
@@ -33,6 +34,90 @@ async def status():
   await bot.change_presence(status=discord.Status.do_not_disturb,activity=discord.Game(random.choice(setting["status"])))
 
 
+def chat_response(input_string):
+    message = re.split(r'\s+|[,;?!.-]\s*', input_string)
+    unlist = "".join(message)
+    split_message = []
+    for i in unlist:
+        cn = list(i)
+        split_message.extend(cn)
+    score_list = []
+
+    # Check all the responses
+    for response in chat_data:
+        response_score = 0
+        required_score = 0
+        required_words = response["required_words"]
+
+        # Check if there are any required words
+        if required_words:
+            for word in message:
+                if word in required_words:
+                    required_score += 1
+                    print(required_score)
+
+        # Amount of required words should match the required score
+        if required_score == len(required_words):
+            # print(required_score == len(required_words))
+            # Check each word the user has typed
+            for word in message:
+                # If the word is in the response, add to the score
+                if word in response["user_input"]:
+                    response_score += 1
+
+        # Add score to list
+        score_list.append(response_score)
+        # Debugging: Find the best phrase
+        print(response_score, response["user_input"])
+
+    # Find the best response and return it if they're not all 0
+    best_response = max(score_list)
+    response_index = score_list.index(best_response)
+
+    # If there is no good response, return a random one.
+    if best_response != 0:
+        return chat_data[response_index]["bot_response"]
+    
+    score_list = []
+
+    # Check all the responses
+    for response in chat_data:
+        response_score = 0
+        required_score = 0
+        required_words = response["required_words"]
+
+        # Check if there are any required words
+        if required_words:
+            for word in split_message:
+                if word in required_words:
+                    required_score += 1
+                    print(required_score)
+
+        # Amount of required words should match the required score
+        if required_score == len(required_words):
+            # print(required_score == len(required_words))
+            # Check each word the user has typed
+            for word in split_message:
+                # If the word is in the response, add to the score
+                if word in response["user_input"]:
+                    response_score += 1
+
+        # Add score to list
+        score_list.append(response_score)
+        # Debugging: Find the best phrase
+        # print(response_score, response["user_input"])
+
+    # Find the best response and return it if they're not all 0
+    best_response = max(score_list)
+    response_index = score_list.index(best_response)
+
+    # If there is no good response, return a random one.
+    if best_response != 0:
+        return random.choice(chat_data[response_index]["bot_response"])
+
+
+
+
 
 @bot.event
 async def on_message(msg):
@@ -40,12 +125,10 @@ async def on_message(msg):
     return
   elif msg.content == "test":
     await msg.channel.send("test!")
-  elif msg.content in chat['早安']['input']:
-    out = random.choice(chat['早安']['output'])
-    await msg.channel.send(out)
-  elif msg.content in chat['晚安']['input']:
-    out = random.choice(chat['晚安']['output'])
-    await msg.channel.send(out)
+  
+  
+  elif chat_response(msg.content):
+    await msg.channel.send(chat_response(msg.content))
   
 
 
