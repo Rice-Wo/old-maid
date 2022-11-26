8b6a87e419c229eb12304e91c7d9d8120d169433
Rice-Wo <yeeho2006@gmail.com>
2022-11-26 12:56:07 +0800

    回覆功能改回完全相通才觸發

--- main.py
+++ main.py
@@ -38,86 +38,10 @@ async def status():
 
 
 def chat_response(input_string):
-  message = re.split(r'\s+|[,;?!.-:]\s*', input_string)
-  unlist = "".join(message)
-  split_message = []
-  for i in unlist:
-      cn = list(i)
-      split_message.extend(cn)
-  score_list = []
-
-  # Check all the responses
   for response in chat_data:
-      response_score = 0
-      required_score = 0
-      required_words = response["required_words"]
-
-      # Check if there are any required words
-      if required_words:
-          for word in message:
-              if word in required_words:
-                  required_score += 1
-                  
-
-      # Amount of required words should match the required score
-      if required_score == len(required_words):
-          # print(required_score == len(required_words))
-          # Check each word the user has typed
-          for word in message:
-              # If the word is in the response, add to the score
-              if word in response["user_input"]:
-                  response_score += 1
-
-      # Add score to list
-      score_list.append(response_score)
-      # Debugging: Find the best phrase
-      # print(response_score, response["user_input"])
-
-  # Find the best response and return it if they're not all 0
-  best_response = max(score_list)
-  response_index = score_list.index(best_response)
-
-  # If there is no good response, return a random one.
-  if best_response != 0:
-      return random.choice(chat_data[response_index]["bot_response"])
-  
-  score_list = []
-
-  # Check all the responses
-  for response in chat_data:
-      response_score = 0
-      required_score = 0
-      required_words = response["required_words"]
-
-      # Check if there are any required words
-      if required_words:
-          for word in split_message:
-              if word in required_words:
-                  required_score += 1
-                  
-
-      # Amount of required words should match the required score
-      if required_score == len(required_words):
-          # print(required_score == len(required_words))
-          # Check each word the user has typed
-          for word in split_message:
-              # If the word is in the response, add to the score
-              if word in response["user_input"]:
-                  response_score += 1
-
-      # Add score to list
-      score_list.append(response_score)
-      # Debugging: Find the best phrase
-      # print(response_score, response["user_input"])
-
-  # Find the best response and return it if they're not all 0
-  best_response = max(score_list)
-  response_index = score_list.index(best_response)
-
-  # If there is no good response, return a random one.
-  if best_response != 0:
-      return random.choice(chat_data[response_index]["bot_response"])
-
+    if input_string in response['user_input']:
+      ans = random.choice(response['bot_response'])
+      return ans
 
 
 
