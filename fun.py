c49cd432f66c8f916296465eb2fefdb1b2a6962a
Rice-Wo <yeeho2006@gmail.com>
2023-08-09 12:26:47 +0800

    style: 將讀寫json方式由絕對路徑改為相對路徑

--- fun.py
+++ fun.py
@@ -13,18 +13,15 @@ logger = logging.getLogger()
 
 
 # 定義全域變數
-main_script_path = os.path.abspath(sys.argv[0])
-main_script_directory = os.path.dirname(main_script_path)
+
 
 def writeJson(file, item): #JSON寫入
-    file_path = os.path.join(main_script_directory, file + '.json')
-    with open(file_path, "w+") as f:
+    with open(file + '.json', "w+") as f:
         f.write(json.dumps(item, ensure_ascii=False, indent=4))
 
 
 def readJson(file): #JSON讀取
-    file_path = os.path.join(main_script_directory, file + '.json')
-    with open(file_path, "r", encoding='utf-8') as f:
+    with open(file + '.json', "r", encoding='utf-8') as f:
         data = json.load(f)
     return data
 
