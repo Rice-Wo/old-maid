0abaa31ef32e213ca94371c67aa2abe94f25f57c
Rice-Wo <yeeho2006@gmail.com>
2023-08-29 10:36:52 +0800

    test: 功能測試新

--- fun.py
+++ fun.py
@@ -21,7 +21,7 @@ sys.excepthook = handle_exception
 def writeJson(file, item): #JSON寫入
     with open(file + '.json', "w+", encoding='utf-8') as f:
         f.write(json.dumps(item, ensure_ascii=False, indent=4))
-    logging.debug(f'寫入資料至 {file}.json 資料內容{item}')
+   # logging.debug(f'寫入資料至 {file}.json 資料內容{item}')
 
 
 def readJson(file): #JSON讀取
--- genshin.py
+++ genshin.py
@@ -35,6 +35,7 @@ class genshin_gacha:
         self.character5 = self.user['character']['5star']
         self.character4 = self.user['character']['4star']
 
+
     def update_data(self):
         self.data[self.id]['total_pulls'] = self.tot_pulls
         self.data[self.id]['pro'] = self.pro
@@ -45,6 +46,7 @@ class genshin_gacha:
         self.data[self.id]['character']['5star'] = self.character5
         self.data[self.id]['character']['4star'] = self.character4
         writeJson('gacha', self.data)
+
 
     def register(self): # 註冊使用者
         self.data[self.id] = {}
@@ -60,8 +62,9 @@ class genshin_gacha:
 
         writeJson('gacha', self.data)
 
+
     def pull_star(self): # 抽出星級
-        pro = self.user['pro']
+        pro = self.pro
         rand_num = random.random()
         addup_prob = 0
 
@@ -70,6 +73,7 @@ class genshin_gacha:
             if rand_num < addup_prob:
                 return star
 
+
     def gacha_system(self): #抽卡本體
         self.tot_pulls += 1
         self.pity5_pulls += 1
@@ -77,21 +81,23 @@ class genshin_gacha:
 
      # 機率改動
         if self.pity5_pulls >= self.setting['五星機率增加']:
-            self.user['pro']['5'] += 0.06
+            self.pro['5'] += 0.06
             writeJson('gacha', self.data)
+        elif self.pity4_pulls >= self.setting['四星機率增加']:
+            self.pro['4'] = self.setting['四星機率up']
         
         star = self.pull_star()        # 取得本次星級
 
         if star == '5':
             self.pity5_pulls = 0
-            self.pro = self.setting['基礎機率']
+            self.pro['5'] = self.setting['基礎機率']['5']
             if self.pity5 == True:
                 up = True
-                print(f'pity5: True')               
+               # logging.debug(f'pity5: True')               
             else:
                 up = random.choice([True, False])
-                print(f'pity5: False')
-            print(f'up5: {up}')
+                #logging.debug(f'pity5: False')
+            #logging.debug(f'up5: {up}')
             if up == True:
                 result = self.setting['up五星']
                 self.pity5 = False
@@ -103,16 +109,43 @@ class genshin_gacha:
                 self.character5[result] = 1
             else:
                 self.character5[result] +=1
-                print(f'5result: {result}')
+            #logging.debug(f'5result: {result}')
 
         if star =='4':
-            pass # W.I.P.
+            self.pity4_pulls = 0
+            self.pro['4'] = self.setting['基礎機率']['4']
+            if self.pity4 == True:
+                up = True
+            #    logging.debug(f'pity4: True')               
+            else:
+                up = random.choice([True, False])
+                #logging.debug(f'pity4: False')
+           # logging.debug(f'up4: {up}')
+            if up == True:
+                result = random.choice(self.setting['up四星'])
+                self.pity4 = False
+                
+            else:
+                result = random.choice(self.setting['常駐四星'])
+                self.pity4 = True
+            if result not in self.character4:
+                self.character4[result] = 1
+            else:
+                self.character4[result] +=1
+            #logging.debug(f'4result: {result}')
 
+        if star == '3':
+            result = random.choice(self.setting['三星'])
+        output = {}
+        output['star'] = star
+        output['result'] = result
+        return output
 
 
     def gacha(self):
-        self.gacha_system()
-        self.update_data() 
+        result = self.gacha_system()
+        self.update_data()
+        return result
 
     
   
