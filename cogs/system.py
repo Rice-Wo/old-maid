29922abf4711a510a8f7587f3f58eb1d821b5850
Rice-Wo <yeeho2006@gmail.com>
2023-10-09 14:59:12 +0800

    refactor: 將程式改為cog

--- cogs/system.py
+++ cogs/system.py
@@ -1,3 +1,16 @@
+import logging.config
+import sys
+from fun import readJson
+
+#log設定
+logging.config.dictConfig(readJson('log_config'))
+logger = logging.getLogger()
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    logger.error("程式碼發生錯誤或例外", exc_info=(exc_type, exc_value, exc_traceback))
+sys.excepthook = handle_exception
+
+
 import discord
 from discord.ext import commands
 from fun import *
