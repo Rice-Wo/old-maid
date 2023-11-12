218723aec9530395fe6ad7de5759a913a12aeed9
Rice-Wo <yeeho2006@gmail.com>
2023-11-12 10:19:16 +0800

    docs: 新增註解

--- fun.py
+++ fun.py
@@ -6,11 +6,13 @@ import discord
 import logging.config
 
 #logging設定
-with open('log_config.json', "r", encoding='utf-8') as f:
+with open('log_config.json', "r", encoding='utf-8') as f: # 讀取log設定json檔案
     log = json.load(f)
+
 logging.config.dictConfig(log)
 logger = logging.getLogger()
-def handle_exception(exc_type, exc_value, exc_traceback):
+
+def handle_exception(exc_type, exc_value, exc_traceback): #設定發生問題時的處理方式
     logger.error("程式碼發生錯誤或例外", exc_info=(exc_type, exc_value, exc_traceback))
 sys.excepthook = handle_exception
 
