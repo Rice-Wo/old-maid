949d5eb676454747bc1ede7398c820b8928f8b77
Rice-Wo <yeeho2006@gmail.com>
2023-11-22 15:31:39 +0800

    docs: 更新需求列表

--- utility/json.py
+++ utility/json.py
@@ -1,12 +1,21 @@
 import json
 
-def writeJson(file, item): #JSON寫入
-    with open(file + '.json', "w+", encoding='utf-8') as f:
+
+
+
+def input_data(file, item): #JSON寫入
+    with open('data/'+ file + '.json', "w+", encoding='utf-8') as f:
         f.write(json.dumps(item, ensure_ascii=False, indent=4))
    # logging.debug(f'寫入資料至 {file}.json 資料內容{item}')
 
-
-def readJson(file): #JSON讀取
-    with open(file + '.json', "r", encoding='utf-8') as f:
+def get_data(file): #JSON讀取
+    with open('data/'+ file + '.json', "r", encoding='utf-8') as f:
         data = json.load(f)
     return data
+
+
+def get_setting(file): #JSON讀取
+    with open('settings/' + file + '.json', "r", encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
