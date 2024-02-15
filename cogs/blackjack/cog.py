89a5d00b174b3cfee29ab9c1ffcc7e9fdf0fbff4
Rice-Wo <yeeho2006@gmail.com>
2024-02-15 12:49:47 +0800

    feat: 新增21點

--- cogs/blackjack/cog.py
+++ cogs/blackjack/cog.py
@@ -3,8 +3,9 @@ from discord.ext import commands
 import requests
 import logging
 
-from .utility import get_data
+from .utility import get_data, list_str, input_data
 from config import config
+from .blackjack import blackjack_game
 
 """
 21點
@@ -16,13 +17,34 @@ class blackjack(commands.Cog):
     def __init__(self, bot): 
         self.bot = bot   
         self.version = config.version
+        self.bj = None
+        self.bj_data = get_data('blackjack')
 
             
-    @discord.slash_command(name="play blackjack")
-    async def ping(self, ctx):
-        await ctx.respond(f"在做了不要催")
+    @discord.slash_command(name="blackjack")
+    async def bj(self, ctx):
+        self.bj = blackjack_game()
+        self.bj.game_start()
+        self.bj_data['tot'] += 1
+        input_data('blackjack', self.bj_data)
+        embed=discord.Embed(title=f'21點', description=f'你的牌：{list_str(self.bj.player_hand)}')
+        await ctx.respond(embed=embed, view=MyView(self.bj))
 
-    
+    @discord.slash_command(Name='blackjack-stats')
+    async def bj_stats(self, ctx):
+        tot = self.bj_data['tot']
+        com = self.bj_data['com']
+        player = self.bj_data['player']
+        both = self.bj_data['both']
+
+        player_win_rate = (player / tot)*100
+        com_win_rate = (com / tot)*100
+        both_rate = (both / tot)*100
+        give_up_rate = ((tot-(player+com+both)) / tot)*100
+        desc = f'玩家勝率：{player_win_rate} \n電腦勝率：{com_win_rate} \n平手機率：{both_rate} \n棄賽率：{give_up_rate}'
+        embed=discord.Embed(title=f'21點統計', description=desc)
+        await ctx.respond(embed=embed)
+
     
 
 
@@ -42,3 +64,44 @@ def setup(bot): # this is called by Pycord to setup the cog
 logging.debug('blackjack had been imported')
 
 
+class MyView(discord.ui.View):
+    def __init__(self, bj):
+        super().__init__()
+        self.bj = bj
+        self.bj_data = get_data('blackjack')
+
+    @discord.ui.button(label="抽牌", row=0, style=discord.ButtonStyle.primary)
+    async def first_button_callback(self, button, interaction):
+        self.bj.game_add_card()
+        player = self.bj.player_point
+        if player > 21:
+            self.bj_data['com'] += 1
+            input_data('blackjack', self.bj_data)
+            embed=discord.Embed(title=f'勝者 電腦', description=f'玩家點數：{player} \n電腦點數：N/A')
+            embed.add_field(name='玩家的牌', value=list_str(self.bj.player_hand), inline=False)
+            await interaction.response.edit_message(embed=embed, view=None)
+        else:
+            embed=discord.Embed(title=f'21點', description=f'你的牌：{list_str(self.bj.player_hand)}')
+            await interaction.response.edit_message(embed=embed, view=self)
+
+    @discord.ui.button(label="放棄", row=0, style=discord.ButtonStyle.primary)
+    async def second_button_callback(self, button, interaction):
+        self.bj.game_end()
+        player = self.bj.player_point
+        com = self.bj.com_point
+        if player == com:
+            self.bj_data['both'] += 1
+            input_data('blackjack', self.bj_data)
+            winner = '平手'
+        elif com > 21 or (21-com) > (21-player):
+            self.bj_data['player'] += 1
+            input_data('blackjack', self.bj_data)
+            winner = '玩家'
+        else:
+            self.bj_data['com'] += 1
+            input_data('blackjack', self.bj_data)
+            winner = '電腦'
+        embed=discord.Embed(title=f'勝者 {winner}', description=f'玩家點數：{player} \n電腦點數：{com}')
+        embed.add_field(name='玩家的牌', value=list_str(self.bj.player_hand), inline=False)
+        embed.add_field(name='電腦的牌', value=list_str(self.bj.computer_hand), inline=True)
+        await interaction.response.edit_message(embed=embed, view=None)
